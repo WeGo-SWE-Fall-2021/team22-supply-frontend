4e6b848e3256bf93a8ef3139cd7bb1cb7a6fdce5
Morgan Prukop <mprukop@stedwards.edu>
2021-03-26 15:58:56 -0500

    test fetch for getting vehicle

---
+++ dashboard.js
@@ -0,0 +1,11 @@
+var fakeVehicles = `[
+    { "id": "1", 
+      "location": "30.256937, -97.74562",
+      "vType": "food"
+       },
+    { "id": "2",
+      "location": "22.256937, -88.74562",
+       "vType": "food"
+        }
+     ]`
+
