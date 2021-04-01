35dc5f0f617dd4e552ea7fded520908247ea7627
ErrorErrorError <erikbautista15@gmail.com>
2021-03-31 19:16:59 -0500

    Testing nginx redirect if cookie is not available

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,14 +4,14 @@ $(() => {
 
     // Handle user data if there is someone is logged in, else redirect them to login page
 
-    fetchLoggedInUser().then(data => {
-        if (data.status === 200) {
-            // Success in fetching data now update dashboard with the new data
-        } else {
-            // Was not able to fetch user
-            window.location.replace(cloudURL + "/login.html")
-        }
-    })
+    // fetchLoggedInUser().then(data => {
+    //     if (data.status === 200) {
+    //         // Success in fetching data now update dashboard with the new data
+    //     } else {
+    //         // Was not able to fetch user
+    //         window.location.replace(cloudURL + "/login.html")
+    //     }
+    // })
 
     // If a user clicks on the logout button, cookies will be empty and will be taken to main page
     $('#logoutButton').click(() => {
